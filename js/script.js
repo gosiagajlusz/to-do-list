f4041861cdb6b4923e637de771f62cb6b16f6c19
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-07-21 19:58:31 +0200

    added new functions

--- js/script.js
+++ js/script.js
@@ -21,8 +21,28 @@
 
     const init = () => {
         render();
+        const form = document.querySelector(".js-form");
+        const addNewTask = () => {
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+            //dlaczego u mnie musi być tutaj tez a nie tylko w bloku, gdzie wywołuję ją?
+            tasks.push({
+                content: newTaskContent,
+            });
+            render();
+        };
 
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+            if (newTaskContent === "") {
+                return;
+            }
+            addNewTask(newTaskContent);
+        });
     };
+
+
     init();
 
-}
+};
