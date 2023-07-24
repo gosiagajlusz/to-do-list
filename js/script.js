f64302ad1827e08cbabcb8d6699a4ce72fa1d9fa
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-07-24 18:40:44 +0200

    fixed all brackets :)

--- js/script.js
+++ js/script.js
@@ -9,9 +9,33 @@
             done: true,
         },
     ];
+    let render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `<li class="list__item ${task.done ? "list__item--done" : ""}">
+           <button class="js-remove">usuń</button>
+            ${task.content}
+    </li>`;
+        }  
+    document.querySelector(".js-tasks").innerHTML = htmlString
+};
+    const removeButtons = document.querySelectorAll(".js-remove");
+    removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+            tasks.splice(index, 1);
+            render();
+        });
+    });
     onFormSubmit = (event) => {
-        //coś w nawiasie zamiast event-konretniej 
         event.preventDefault();
+        const addNewTask = () => {
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+            tasks.push({
+                content: newTaskContent,
+            });
+            render();
+        };
+
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
         if (newTaskContent === "") {
             return;
@@ -19,24 +43,7 @@
         addNewTask(newTaskContent);
     };
 
-    const addNewTask = () => {
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
-        tasks.push({
-            content: newTaskContent,
-        });
-        render();
-    };
 
-
-    let render = () => {
-        let htmlString = "";
-        for (const task of tasks) {
-            htmlString += `<li class="list__item ${task.done ? "list__item--done" : ""}">
-            ${task.content}
-    </li>`;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString
-    };
     const init = () => {
         render();
         const form = document.querySelector(".js-form");
