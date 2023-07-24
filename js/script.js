0384c2b195f34e663ee8673331f21f1943cc4ac0
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-07-24 12:33:34 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -9,6 +9,25 @@
             done: true,
         },
     ];
+    onFormSubmit = (event) => {
+        //coś w nawiasie zamiast event-konretniej 
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+    };
+
+    const addNewTask = () => {
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+
     let render = () => {
         let htmlString = "";
         for (const task of tasks) {
@@ -18,31 +37,10 @@
         }
         document.querySelector(".js-tasks").innerHTML = htmlString
     };
-
     const init = () => {
         render();
         const form = document.querySelector(".js-form");
-        const addNewTask = () => {
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            //dlaczego u mnie musi być tutaj tez a nie tylko w bloku, gdzie wywołuję ją?
-            tasks.push({
-                content: newTaskContent,
-            });
-            render();
-        };
-
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            if (newTaskContent === "") {
-                return;
-            }
-            addNewTask(newTaskContent);
-        });
+        form.addEventListener("submit", onFormSubmit);
     };
-
-
     init();
-
 };
